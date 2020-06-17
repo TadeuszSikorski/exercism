1cfd835439e1d002546f51df0c1d6398d3f5b0fe
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-06-17 21:46:13 +0200

    adding pangram exercise and solving it

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return false;
+        }
+        else
+        {
+            return alphabet.All(character => input.ToLower().Contains(character));
+        }
+        
+    }
+}
