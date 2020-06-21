98b6281c37057827edf0cd19b99ef101627e5e10
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-06-21 10:32:01 +0200

    correction of the gigasecond exercise solution

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -4,6 +4,6 @@ public static class Gigasecond
 {
     public static DateTime Add(DateTime moment)
     {    
-        return moment + new TimeSpan(0,0,1000000000);
+        return moment.AddSeconds(10e8);
     }
 }
