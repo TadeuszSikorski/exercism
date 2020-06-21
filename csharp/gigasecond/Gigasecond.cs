f26dd125cd52b4b323aadd2c677fe853f0a78c2b
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-06-21 10:48:28 +0200

    improve code to make it more readable and self-documenting

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,8 +2,6 @@ using System;
 
 public static class Gigasecond
 {
-    public static DateTime Add(DateTime moment)
-    {    
-        return moment.AddSeconds(10e8);
-    }
+    public static DateTime Add(DateTime moment) => moment.AddSeconds(10e8);
+    
 }
