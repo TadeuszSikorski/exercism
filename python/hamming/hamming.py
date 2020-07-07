9f32f03c9926f2c8e45c27a6b983c26d46242ca2
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-07-07 22:17:36 +0200

    fix python hamming solution

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,11 +1,11 @@
 def distance(strand_a: str, strand_b: str) -> int:
     distance = 0
 
-    if len(strand_a) == len(strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Sequences are different")
+    else:
         for a, b in zip(strand_a, strand_b):
             if a != b:
                 distance += 1
-    else:
-        raise ValueError("Sequences are different")
 
     return distance
