2df9bb0ca418370a2b60040a1485007eea4797b6
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-07-07 22:19:07 +0200

    fix python matrix solution

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -6,25 +6,15 @@ class Matrix:
         self.matrix = [list(map(int, row.split())) for row in matrix_string.split("\n")]
 
     def row(self, index):
-        self.check_entered_arg(index)
+        self.__check_entered_arg(index)
 
-        self.row_of_matrix = []
-
-        for column_index in range(len(self.matrix[index - 1])):
-            self.row_of_matrix.append(self.matrix[index - 1][column_index])
-
-        return self.row_of_matrix
+        return self.matrix[index - 1]
 
     def column(self, index):
-        self.check_entered_arg(index)
+        self.__check_entered_arg(index)
 
-        self.column_of_matrix = []
+        return [row[index - 1] for row in self.matrix]
 
-        for row_index in range(len(self.matrix)):
-            self.column_of_matrix.append(self.matrix[row_index][index - 1])
-
-        return self.column_of_matrix
-
-    def check_entered_arg(self, arg):
+    def __check_entered_arg(self, arg):
         if type(arg) != int:
             raise Exception("The argument provided must be an integer.")
