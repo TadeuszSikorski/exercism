6734bbc0426908a831b2e4416f6e0ae02932257f
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-07-24 21:12:30 +0200

    fix python grade-school solution

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 class School:
     def __init__(self):
         self.students_roster = []
@@ -6,13 +8,13 @@ class School:
         self.students_roster.append((name, grade))
 
     def roster(self):
-        self.students_roster.sort(key=lambda roster: roster[0])
-        self.students_roster.sort(key=lambda roster: roster[1])
+        self.students_roster.sort(key = itemgetter(0))
+        self.students_roster.sort(key = itemgetter(1))
 
         return [student_roster[0] for student_roster in self.students_roster]
 
     def grade(self, grade_number):
-        self.students_roster.sort(key=lambda roster: roster[0])
+        self.students_roster.sort(key = itemgetter(0))
 
         return [
             student_roster[0]
