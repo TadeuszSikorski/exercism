bdf778ac4d282910f627a53808767646b66e50be
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-07-24 20:08:43 +0200

    add csharp nucleotide-count exercise and solving it

---
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class NucleotideCount
+{
+    public static IDictionary<char, int> Count(string sequence) =>
+        !sequence.All(nucleotide => "ACGT".Contains(nucleotide)) 
+        ? throw new ArgumentException() 
+        : "ACGT".ToDictionary(nucleotide => nucleotide, nucleotide => sequence.Count(character => character == nucleotide));
+}
