cfc9c7563a3090d50d31db91deed827e00d2fe03
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-07-24 21:16:21 +0200

    fix python kindergarten-garden solution

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,51 +1,45 @@
 class Garden:
-    def __init__(self, diagram: str, students=None):
-        self.diagram = diagram
+    __students = [
+        "Alice",
+        "Bob",
+        "Charlie",
+        "David",
+        "Eve",
+        "Fred",
+        "Ginny",
+        "Harriet",
+        "Ileana",
+        "Joseph",
+        "Kincaid",
+        "Larry",
+    ]
 
+    __plants = {
+        "C": "Clover",
+        "G": "Grass",
+        "R": "Radishes",
+        "V": "Violets",
+    }
+
+    def __init__(self, diagram, students=None):
         if students is None:
-            students = [
-                "Alice",
-                "Bob",
-                "Charlie",
-                "David",
-                "Eve",
-                "Fred",
-                "Ginny",
-                "Harriet",
-                "Ileana",
-                "Joseph",
-                "Kincaid",
-                "Larry",
+            students = self.__students
+
+        students = sorted(students)
+        rows = diagram.splitlines()
+        __students_plants = []
+
+        for index, name in enumerate(students):
+            __rows = [
+                [self.__plants[row[index * 2]], self.__plants[row[(index * 2) + 1]]]
+                for row in rows
+                if (index * 2) + 1 < len(rows[0])
             ]
+            __students_plants.append((name, [plant for row in __rows for plant in row]))
 
-        self.students = sorted(students)
-
-        self.plants_names = {
-            "C": "Clover",
-            "G": "Grass",
-            "R": "Radishes",
-            "V": "Violets",
+        self.students_plants = {
+            student_name: plants for (student_name, plants) in __students_plants
         }
 
-    def plants(self, student: str) -> list:
-        rows = self.diagram.split("\n")
-        cups = [
-            [
-                (self.plants_names[row[index]], self.plants_names[row[index + 1]])
-                for index in range(len(row))
-                if index % 2 == 0 and index + 1 < len(row)
-            ]
-            for row in rows
-        ]
-        students_plants = [[] for index in range(len(self.students))]
-
-        for row_cups in cups:
-            for index in range(len(row_cups)):
-                students_plants[index].append(row_cups[index])
-
-        students_plants = [
-            [plant for plants in student_plants for plant in plants]
-            for student_plants in students_plants
-        ]
-
-        return students_plants[self.students.index(student)]
+    def plants(self, student):
+        return self.students_plants[student]
