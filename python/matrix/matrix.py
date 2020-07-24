71b5e80fc1fa0454a86a9891dbe50347c3d76f4f
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-07-24 21:17:00 +0200

    fix python matrix solution

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,20 +1,9 @@
 class Matrix:
     def __init__(self, matrix_string):
-        if type(matrix_string) != str:
-            raise Exception("The argument provided must be a string.")
-
         self.matrix = [list(map(int, row.split())) for row in matrix_string.split("\n")]
 
     def row(self, index):
-        self.__check_entered_arg(index)
-
         return self.matrix[index - 1]
 
     def column(self, index):
-        self.__check_entered_arg(index)
-
         return [row[index - 1] for row in self.matrix]
-
-    def __check_entered_arg(self, arg):
-        if type(arg) != int:
-            raise Exception("The argument provided must be an integer.")
