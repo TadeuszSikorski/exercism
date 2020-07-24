3966d0496ea580e6f12fdc96571e95f14c86651d
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-07-24 21:06:38 +0200

    fix python markdown solution

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -1,55 +1,20 @@
 import re
 
 
-def get_headings(markdown: str) -> str:
-    for number in range(1, 7):
-        markdown = re.sub(
-            r"^{} (.*?$)".format("#" * number),
-            r"<h{0}>\1</h{0}>".format(number),
-            markdown,
-            flags=re.M,
-        )
-
-    return markdown
-
-
-def get_important_texts(markdown: str) -> str:
-    return re.sub(r"__(.*)__", r"<strong>\1</strong>", markdown)
-
-
-def get_emphasized_texts(markdown: str) -> str:
-    return re.sub(r"_(.*)_", r"<em>\1</em>", markdown)
-
-
-def get_ordered_lists(markdown: str) -> str:
-    return re.sub(r"^\* (.*)", r"<li>\1</li>", markdown, flags=re.M)
-
-
-def get_unordered_lists(markdown: str) -> str:
-    return re.sub(r"(\n?<li>.*</li>)", r"<ul>\1</ul>", markdown, flags=re.S)
-
-
-def get_paragraphs(markdown: str) -> str:
-    return re.sub(r"^(?!<[hlu])(.*?$)", r"<p>\1</p>", markdown, flags=re.M)
-
-
-def delete_new_line(markdown: str) -> str:
-    return re.sub(r"\n", "", markdown)
+IMPORTANT_TEXT = re.compile(r"__(.*)__")
+EMPHASIZED_TEXT = re.compile(r"_(.*)_")
+ORDERED_LIST = re.compile(r"^\* (.*)", flags=re.MULTILINE)
+HEADER = re.compile(r"^(#{1,6}) (.*)$", flags=re.MULTILINE)
+UNORDERED_LIST = re.compile(r"(\n?<li>.*</li>)", flags=re.S)
+PARAGRAPH = re.compile(r"^(?!<[hlu])(.*?$)", flags=re.MULTILINE)
 
 
 def parse(markdown):
-    markdown = get_important_texts(markdown)
-
-    markdown = get_emphasized_texts(markdown)
-
-    markdown = get_ordered_lists(markdown)
-
-    markdown = get_headings(markdown)
-
-    markdown = get_unordered_lists(markdown)
-
-    markdown = get_paragraphs(markdown)
-
-    markdown = delete_new_line(markdown)
-
-    return markdown
+    markdown = IMPORTANT_TEXT.sub(r"<strong>\1</strong>", markdown)
+    markdown = EMPHASIZED_TEXT.sub(r"<em>\1</em>", markdown)
+    markdown = ORDERED_LIST.sub(r"<li>\1</li>", markdown)
+    number = 0 if not HEADER.search(markdown) else len(HEADER.search(markdown).group(1))
+    markdown = (markdown if number == 0 else HEADER.sub(f"<h{number}>" + r"\2" + f"</h{number}>", markdown))
+    markdown = UNORDERED_LIST.sub(r"<ul>\1</ul>", markdown)
+    markdown = PARAGRAPH.sub(r"<p>\1</p>", markdown)
+    return "".join(markdown.splitlines())
