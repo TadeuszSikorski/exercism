52aa99ff10bdc6a1390e4e99f7163614b284678e
Tadeusz Sikorski <sikorski.tadeusz@yahoo.com>
2020-07-24 21:15:08 +0200

    fix python scrabble-score solution

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,39 +1,24 @@
-characters_values: dict = {
-    "A": 1,
-    "E": 1,
-    "I": 1,
-    "O": 1,
-    "U": 1,
-    "L": 1,
-    "N": 1,
-    "R": 1,
-    "S": 1,
-    "T": 1,
-    "D": 2,
-    "G": 2,
-    "B": 3,
-    "C": 3,
-    "M": 3,
-    "P": 3,
-    "F": 4,
-    "H": 4,
-    "V": 4,
-    "W": 4,
-    "Y": 4,
-    "K": 5,
-    "J": 8,
-    "X": 8,
-    "Q": 10,
-    "Z": 10
+LETTERS_AND_SCORES = {
+    1: "A, E, I, O, U, L, N, R, S, T",
+    2: "D, G",
+    3: "B, C, M, P",
+    4: "F, H, V, W, Y",
+    5: "K",
+    8: "J, X",
+    10: "Q, Z",
+}
+
+LETTERS_TO_SCORES = {
+    letter: score
+    for score, letters in LETTERS_AND_SCORES.items()
+    for letter in letters.split(", ")
 }
 
 
 def score(word: str) -> int:
     final_score: int = 0
 
-    for char in word.upper():
-        for character, value in characters_values.items():
-                if char == character:
-                    final_score += value
+    for character in word.upper():
+        final_score += LETTERS_TO_SCORES[character]
 
     return final_score
